1c5b8d34e9f41987c32ca0d869d94693ee88b1bd
Nugie kurniawan <nugiebinhar@gmail.com>
2025-06-15 22:27:04 +0700

    Membuat fitur login dan berjalan dengan baik

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,8 @@
 <div class="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md">
     <h2 class="text-2xl font-bold text-gray-800 text-center mb-6">Login to Your Account</h2>
 
-    <form action="#" method="POST" class="space-y-5">
+    <form action="{{ route("login.post") }}" method="POST" class="space-y-5">
+        @csrf
       <div>
         <label for="email" class="block text-sm font-medium text-gray-700 mb-1">Email</label>
         <input type="email" name="email" id="email" required class="w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500" placeholder="you@example.com" />
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,30 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
-});
+})->name("home")->middleware('auth');
 Route::get("/login",function(){
-    return view("auth.login");
-});
+    if(Auth::check()){
+        return redirect()->intended('home');
+    }
+    return view('auth.login');
+})->name("login");
+Route::post("/login",function(Request $request){
+    $credential = $request->validate([
+        "email" => "required|email",
+        "password" => "required"
+    ]);
+    if(Auth::attempt($credential)){
+        $request->session()->regenerate();
+        return redirect()->intended('home');
+    }
+    return back()->withErrors(["gagal" => "gagal login"]);
+})->name("login.post");
+Route::get("/register",fn()=>view("auth.register"))->name("register");
+// Route::post('users/{id}', function ($id) {
+
+// });
