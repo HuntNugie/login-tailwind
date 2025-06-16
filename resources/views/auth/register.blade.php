aa39a2da271bdd9ec5546613932fc554fc67591b
Nugie kurniawan <nugiebinhar@gmail.com>
2025-06-16 10:21:35 +0700

    Membuat fitur register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,8 +4,9 @@
  <div class="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md">
     <h2 class="text-2xl font-bold text-gray-800 text-center mb-6">Create an Account</h2>
 
-    <form action="#" method="POST" class="space-y-5">
-      <div>
+    <form action="{{ route("register.store") }}" method="POST" class="space-y-5">
+        @csrf
+        <div>
         <label for="name" class="block text-sm font-medium text-gray-700 mb-1">Full Name</label>
         <input type="text" name="name" id="name" required class="w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none" placeholder="Your name"/>
       </div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
+Route::get("/",fn()=> redirect()->route("home"));
 Route::get('/home', function () {
     return view('welcome');
 })->name("home")->middleware('auth');
@@ -25,11 +28,29 @@ Route::post("/login",function(Request $request){
     return back()->withErrors(["gagal" => "gagal login"]);
 })->name("login.post");
 Route::get("/register",fn()=>view("auth.register"))->name("register");
+Route::post("/register",function(Request $request){
+    $credential = $request->validate([
+        "name" => "required | string",
+        "email" => "required | email | unique:users",
+        "password" => "required"
+    ]);
+
+    if($request->password !== $request->confirm_password){
+        return back()->withErrors(["gagal" => "Confirmasi password tidak sama dengan password"]);
+    }
+   $user =  User::create([
+        "name" => $request->name,
+        "email" => $request->email,
+        "password" => Hash::make($request->password)
+    ]);
+    Auth::login($user);
+
+    return redirect()->intended('home');
+})->name("register.store");
 Route::post("/logout",function(){
     Auth::logout();
     session()->invalidate();
     return redirect()->route("login");
 })->name("logout");
-// Route::post('users/{id}', function ($id) {
 
-// });
+
